1d37d8fe3cfcef7a1902a1a0621de5369c34a50b
Guosa Edebiri <gedebiri@gmail.com>
2020-04-17 19:07:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split{ }
-  reversed_string_array = string_array.each do |element| element.reverse!
+  reversed_string_array = string_array.each do {|element| element.reverse!
   end
   reversed_string_array.join
 end
