f86338b19d57eb522ba0f162e5225bc5805594eb
Guosa Edebiri <gedebiri@gmail.com>
2020-04-17 19:23:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  string.split(" ").collect do |element| element.reverse!
+  string.split(" ").collect do {|element| element.reverse!}.join
 end
 end
